80748e660d4b48290399cba21351319ce246be59
carvalhocaio <caiocarvalho.py@gmail.com>
2021-04-25 01:53:28 -0400

    Interfaces

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,5 +1,9 @@
-export class SistemaAutenticacao{
-  static login(funcionario, senha){
-    return funcionario.senha === senha;
+/*
+* Ser autenticavel significa ter o método autenticar
+* */
+
+export class SistemaAutenticacao {
+  static login (autenticavel, senha) {
+    return autenticavel.autenticar(senha)
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Gerente } from './Funcionario/Gerente.js'
 import { Diretor } from './Funcionario/Diretor.js'
 import { SistemaAutenticacao } from './SistemaAutenticacao.js'
+import { Cliente } from './Cliente.js'
 
 
 const diretor = new Diretor('Rodrigo', 10000, 12345678900)
@@ -8,5 +9,9 @@ diretor.cadastrarSenha('123456')
 const gerente = new Gerente('Ricardo', 5000, 12378945601)
 gerente.cadastrarSenha('123')
 
-const estaLogado = SistemaAutenticacao.login(gerente, '123')
-console.log(estaLogado)
+const cliente = new Cliente('Lais', 78945612379, '456')
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123')
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456')
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456')
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado)
